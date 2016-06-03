a653efe1b79a6e5c9b02e25199fccf8100f817d7
Daniel Eichhorn <dani.eichhorn@squix.ch>
2016-06-03 14:35:54 +0200

    Clean up and enable auto check

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
-#include <Wire.h>
-#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-//needed for library
+
+#include <ESP8266WiFi.h>
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
-#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
-
-
+#include <WiFiManager.h>
 #include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
-#include "ESP8266httpUpdate.h"
+#include <ESP8266httpUpdate.h>
+#include <Ticker.h>
+
 #include "version.h"
 
 ESP8266WiFiMulti WiFiMulti;
+Ticker updateCheck;
+boolean doUpdateCheck = false;
+
+void enableUpdateCheck() {
+  doUpdateCheck = true;
+}
 
 void setup() {
 
@@ -30,12 +34,15 @@ void setup() {
     //wifiManager.autoConnect();
     Serial.println("Started..");
     Serial.println("BUILD_TAG: "+ String(BUILD_TAG));
+
+    // don't wanna miss a thing... Check every 60 seconds
+    updateCheck.attach(60, enableUpdateCheck);
 }
 
 void loop() {
     // press nodemcu's flash button
-    int result0 = digitalRead(0);
-    if (result0 == LOW) {
+    int flashButtonState = digitalRead(0);
+    if (flashButtonState == LOW || doUpdateCheck) {
       Serial.println("Going to update firmware...");
       if((WiFiMulti.run() == WL_CONNECTED)) {
 
@@ -56,7 +63,8 @@ void loop() {
                       break;
               }
           }
+          doUpdateCheck = false;
       }
-//    }
+
 
 }
